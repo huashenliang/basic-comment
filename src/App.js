e857fa13399deea83a87514e39fc8d9749863105
John <John@brandfire.ca>
2021-03-12 13:13:08 -0500

    Basic comment system

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Container, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Comment = (props) => {
+  const [currentComment, setCurrentComment] = useState('');
+  const [commentArr, setCommentArr] = useState([]);
+
+  const postComment = (currentComment) => {
+    setCommentArr([...commentArr, currentComment])
+  }
+
+  return (
+    <div className="container">
+      {props.title ?
+        <div>
+          <h3>comment: {props.title}</h3>
+          <input className="mr-3" type="text" placeholder="your comment" value={currentComment} onChange={e => setCurrentComment(e.target.value)} />
+          <Button onClick={() => postComment(currentComment)}>Post Reply</Button>
+
+          <Container>
+            {commentArr.map(i =>
+              <Comment title={i} />
+            )}
+          </Container>
+        </div>
+        : ""
+      }
+    </div>
+  )
+}
 
 function App() {
+  const [newComment, setNewComment] = useState('');
+  const [commentArr, setCommentArr] = useState([]);
+
+  const postComment = (newComment) => {
+    setCommentArr([...commentArr, newComment])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Comments:</h1>
+      {commentArr.map((i, index) =>
+        <Comment title={i} key={index} />
+      )
+      }
+      <div className="mt-5">
+        <h2>Post new comment here:</h2>
+        <input className="mr-3" type="text" placeholder="your comment" value={newComment} onChange={e => setNewComment(e.target.value)} />
+        <Button onClick={() => postComment(newComment)}>Post new comment</Button>
+      </div>
+    </Container>
   );
 }
 
